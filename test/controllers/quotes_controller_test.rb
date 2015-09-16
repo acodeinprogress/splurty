a34af2adeb95bfcaa775e6d4070c78dd8ab5c9a9
Sharon Siegel <sjsieg830@gmail.com>
2015-09-16 19:27:36 +0000

    Add show page and controller tests

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class QuotesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "quote show page" do
+    quote = Quote.create(:author => "Irving Berlin", :saying => "Life is 10 percent what you make it, and 90 percent how you take it.")
+    get :show, :id => quote.id
+    assert_response :success
+  end
+
+  test "quote show page, not found" do
+    get :show, :id => 'OMG'
+    assert_response :not_found
+  end
 end
